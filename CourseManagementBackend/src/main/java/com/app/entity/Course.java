08da110770e624cda2b7736126936048968e5917
D5_83927_Adhishri <adhishrishinde2001@gmail.com>
2025-08-27 23:24:55 +0530

    Course error resolved

--- CourseManagementBackend/src/main/java/com/app/entity/Course.java
+++ CourseManagementBackend/src/main/java/com/app/entity/Course.java
@@ -48,7 +48,7 @@ public class Course extends BaseEntity {
 
 	@OneToMany(fetch = FetchType.EAGER)
 	@JoinColumn(name = "premise_id")
-	private List<Premises> premises;
+	private Premises premises;
 
 	@ManyToOne
 	@JoinColumn(name = "course_type_id")
