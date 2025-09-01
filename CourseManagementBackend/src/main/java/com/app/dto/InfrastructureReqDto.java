726adddd096c6babf7c70c9a65a7ef7c7d2110eb
Vaibhav <vaibhave52@gmail.com>
2025-09-01 19:55:09 +0530

    Fix InfrastructureReqDto after merge

--- CourseManagementBackend/src/main/java/com/app/dto/InfrastructureReqDto.java
+++ CourseManagementBackend/src/main/java/com/app/dto/InfrastructureReqDto.java
@@ -8,24 +8,15 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-<<<<<<<< HEAD:CourseManagementBackend/src/main/java/com/app/dto/CourseTypeReqDto.java
-public class CourseTypeReqDto {
-========
 @AllArgsConstructor
 public class InfrastructureReqDto {
->>>>>>>> BE-Feature-Infrastructure:CourseManagementBackend/src/main/java/com/app/dto/InfrastructureReqDto.java
 
 	private String title;
 
 	private String description;
-<<<<<<<< HEAD:CourseManagementBackend/src/main/java/com/app/dto/CourseTypeReqDto.java
-	
-}
-========
 
 	private String infrastructureType;
 
 	private Integer premisesId;
 
 }
->>>>>>>> BE-Feature-Infrastructure:CourseManagementBackend/src/main/java/com/app/dto/InfrastructureReqDto.java
