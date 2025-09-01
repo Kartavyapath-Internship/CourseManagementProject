abcfd77b8ef36e7b1cdf2388a7bb360dfa2917f6
Vaibhav <vaibhave52@gmail.com>
2025-09-01 20:07:10 +0530

    Restore CourseTypeReqDto from main version

--- CourseManagementBackend/src/main/java/com/app/dto/CourseTypeReqDto.java
+++ CourseManagementBackend/src/main/java/com/app/dto/CourseTypeReqDto.java
@@ -4,28 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@Setter
 @Getter
-@NoArgsConstructor
-<<<<<<<< HEAD:CourseManagementBackend/src/main/java/com/app/dto/CourseTypeReqDto.java
-public class CourseTypeReqDto {
-========
+@Setter
+@ToString
 @AllArgsConstructor
-public class InfrastructureReqDto {
->>>>>>>> BE-Feature-Infrastructure:CourseManagementBackend/src/main/java/com/app/dto/InfrastructureReqDto.java
+@NoArgsConstructor
+public class CourseTypeReqDto {
 
 	private String title;
 
 	private String description;
-<<<<<<<< HEAD:CourseManagementBackend/src/main/java/com/app/dto/CourseTypeReqDto.java
 	
 }
-========
-
-	private String infrastructureType;
-
-	private Integer premisesId;
-
-}
->>>>>>>> BE-Feature-Infrastructure:CourseManagementBackend/src/main/java/com/app/dto/InfrastructureReqDto.java
