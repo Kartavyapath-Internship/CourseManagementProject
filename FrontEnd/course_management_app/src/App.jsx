56678042474774db51e7803c5301cf9171901283
Vaibhav <vaibhave52@gmail.com>
2025-09-02 21:17:31 +0530

    React Frontend Basic Structure

---
+++ FrontEnd/course_management_app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold text-blue-500">
+      Hello Tailwind v3 + Vite!
+    </div>
+  );
+  
+}
+
+export default App
