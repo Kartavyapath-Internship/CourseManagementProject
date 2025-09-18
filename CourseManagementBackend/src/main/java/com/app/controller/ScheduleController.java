a53d320a8b61e8c7b6466ecce40b920afd9ed144
D5_83927_Adhishri <adhishrishinde2001@gmail.com>
2025-09-18 22:45:30 +0530

    Cross origin added

--- CourseManagementBackend/src/main/java/com/app/controller/ScheduleController.java
+++ CourseManagementBackend/src/main/java/com/app/controller/ScheduleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import com.app.service.ScheduleService;
 
 @RestController
 @RequestMapping("/api/schedules")
+@CrossOrigin("*")
 public class ScheduleController {
 
     @Autowired
