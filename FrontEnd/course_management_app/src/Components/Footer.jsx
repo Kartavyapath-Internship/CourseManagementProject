14bf8499ab341f904c30076d521ebcedec16c8d2
Vaibhav <vaibhave52@gmail.com>
2025-09-19 08:30:25 +0530

    updated footer

--- FrontEnd/course_management_app/src/Components/Footer.jsx
+++ FrontEnd/course_management_app/src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 text-center p-4 shadow-inner">
-      <p className="text-sm text-gray-600">
+    <footer className="bg-gray-200 text-center p-5 shadow-inner">
+      <p className="text-sm text-gray-800">
         © {new Date().getFullYear()} Course Management System. All rights
         reserved.
       </p>
